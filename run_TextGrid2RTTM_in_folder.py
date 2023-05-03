7c73db4f42fbb28cd3150fa40d654c66a69da2e4
Valeriia Dlugash <lera.dlugash@gmail.com>
2023-05-03 16:44:37 +0300

    removed extention duplicate in the file name

--- run_TextGrid2RTTM_in_folder.py
+++ run_TextGrid2RTTM_in_folder.py
@@ -6,11 +6,12 @@ def process_files_in_folder(folder_path):
             base_name, _ = os.path.splitext(file_name)
             rttm_file = base_name + '.rttm'
             textgrid_file = os.path.join(folder_path, file_name)
-            output_file = os.path.join(folder_path, rttm_file)
+            output_folder_path = '/Users/valeriia/stuff/STUDYING/YEAR_6/masters_degree/rttms_made_of_TextGrids'
+            output_file = os.path.join(output_folder_path, base_name)
             os.system(f"python textgrid2rttm.py {textgrid_file} {output_file}")
 
 # Specify the folder path
-folder_path = "/path/to/folder"
+folder_path = "/Users/valeriia/stuff/STUDYING/YEAR_6/masters_degree/paired_plain_data"
 
 # Call the process_files_in_folder function
 process_files_in_folder(folder_path)
